a6c8f8a4ae6b6a2f375667c46b96b93a6aae9b07
William A <wsashbee@gmail.com>
2021-11-11 22:35:15 -0500

    working 2d

--- 2d-train.py
+++ 2d-train.py
@@ -18,11 +18,19 @@ import numpy as np
 from numpy.core.function_base import linspace
 from numpy.linalg import linalg
 
-xbaseline = np.random.randn(2, 1000)
-
-useGPU = False
+sf = .99999
 mini_batch = 100
-train_size = mini_batch*1
+#xbaseline = np.random.randn(2, 1000)
+
+theta = np.linspace(0.0, 2*3.14159, num=1000)
+x  = np.cos(theta)
+y  = np.sin(theta)
+xbaseline = np.zeros((2,1000))
+xbaseline[0,:] = x
+xbaseline[1,:] = y
+
+useGPU = True
+train_size = mini_batch*80
 
 def vmf(mu, kappa, x):
     # single point function
@@ -41,21 +49,12 @@ def apply_vmf(x, mu, kappa, norm=1.0):
 
 def createDataset(pltFig=True, numFigs=100, numPoints=1000):
     dimensions = 2
-    # x = np.random.randn(numFigs,dimensions, numPoints)
-    ##create an ordered circle instead of a permuted circle
     x = np.zeros((numFigs, dimensions,
                   numPoints))  # this could get interesting! why does it fail on the random permuted version
     xnormed = xbaseline
     xnormed = xnormed / np.linalg.norm(xnormed, axis=0)
     x[:, :, :] = xnormed
-    # theta = np.linspace(0, 2.0*3.14, num=1000)
-    # xx = np.cos(theta)
-    # yy = np.sin(theta)
-    # x[:,0,:]= xx
-    # x[:,1,:]= yy
-    # assert np.sum(x[0,0,:]!=x[1,0,:])==0
-    # assert np.sum(x[0,1,:]!=x[1,1,:])==0
-
+    
     for k in range(numFigs):
         # plt.clf()
         if pltFig:
@@ -67,7 +66,7 @@ def createDataset(pltFig=True, numFigs=100, numPoints=1000):
         xx = x[k, :, :] * w[0]
         it = 10
         for i in range(it):
-            kappa = np.random.randint(5, 100)
+            kappa = 50#np.random.randint(5, 100)
             mu = np.random.randn(2);
             mu = mu / np.linalg.norm(mu)
             y = apply_vmf(xnormed, mu, kappa)
@@ -81,9 +80,6 @@ def createDataset(pltFig=True, numFigs=100, numPoints=1000):
             plt.show()
 
     return x
-
-
-
 
 global numpoints
 numpoints = 1000
@@ -100,19 +96,28 @@ for i in range(32):
 def convex_combination_matrix(length=10):
     canvas = torch.zeros((length, side, side))
     points = createDataset(pltFig=False, numFigs=length, numPoints=1000).transpose(0, 2, 1)
-    maxP = np.max(points)
-    minP = np.min(points)
-    points = (points - minP) * 26.0 / (maxP - minP) + 3.0
+    
+    
+    #print(np.expand_dims(np.min(points, axis=1), axis=1).shape)
+    #print(np.min(points, axis=1).shape)
+    assert np.expand_dims(np.min(points, axis=1), axis=1).shape == (length,1,2)
+    assert np.min(points, axis=1).shape == (length,2)
+    points = (points - np.expand_dims(np.min(points, axis=1), axis=1)) * np.expand_dims(
+            1.0 / (np.max(points, axis=1) - np.min(points, axis=1)), axis=1)
     points = torch.from_numpy(points)
-
+    points = points*sf
+        
+        
+    
+    
     assert points.shape == (length, 1000, 2)
 
     for l in range(length):
-        canvas[l, points[l, :, 0].type(torch.LongTensor), points[l, :, 1].type(torch.LongTensor)] = 1.0
+        x = points[l,:,0]
+        y = points[l,:,1]
 
-        # points[l,:,0] = x[l,:]#modified for lstm discriminator
-        # points[l,:,1] = y[l,:]#modified for lstm discriminator
-
+        canvas[l, (x*side*sf).type(torch.LongTensor), (y*side*sf).type(torch.LongTensor)] = 1.0
+    
     print('canvshape', canvas.shape)
     print('sumcanvas', torch.sum(canvas) / length)
 
@@ -132,7 +137,7 @@ def plot_one(img, xs, ys, i=0):
     assert len(s) == predres
     c = ['red' for x in range(predres)]
     assert len(c) == predres
-    plt.plot(xs.cpu().numpy(), ys.cpu().numpy(), ',', color='red', ms=.3, lw=.3)
+    plt.plot(xs.cpu().numpy()*side*sf, ys.cpu().numpy()*side*sf, ',', color='red', ms=.3, lw=.3)
     # plt.gca().add_artist(ascatter)
     plt.axis('off')
 
@@ -153,22 +158,17 @@ def plot_all(sample=None, model=None, labels=None, i=0):
 
             print('loss', loss)
 
-            for i in range(100):
+            for i in range(mini_batch):
                 img = sample[i, 0, :, :].squeeze().cpu().numpy()
                 X = out[i, :1000, 0]
                 Y = out[i, -1000:, 1]
                 plot_one(img, X, Y, i=i)
-
-
     else:
         print(sample.shape, labels.shape)
-        for i in range(100):
+        for i in range(mini_batch):
             img = sample[i, 0, :, :].squeeze().cpu().numpy()
-            # print(np.sum('img',img))
             X = labels[i, :, 0]
             Y = labels[i, :, 1]
-            # print('x',np.max(X,dim=None),np.min(X,dim=None))
-            # print('y',np.max(Y,dim=None),np.min(Y,dim=None))
             plot_one(img, X, Y, i=i)
 
 
@@ -197,7 +197,6 @@ class DonutDataset(torch.utils.data.Dataset):
 
         canvas = canvas[idx, :, :]
 
-        # assert canvas.shape == (side,side) or canvas.shape == (len(idx))
 
         points = self.values["points"]
         points = points[idx, :]
@@ -216,7 +215,7 @@ class DonutDataset(torch.utils.data.Dataset):
             plot_all(sample=sample, model=model, labels=labels)
             break
         plt.savefig(title, dpi=600)
-
+        plt.clf()
 
 dataset = DonutDataset(length=100)
 
@@ -231,7 +230,6 @@ DonutDataset.displayCanvas('donut.png', loader_demo, model=None)
 from torch.utils import data
 from torch.utils.data import DataLoader, TensorDataset, RandomSampler
 
-mini_batch = 100
 train_dataset = DonutDataset(length = train_size)
 loader_train = data.DataLoader(
     train_dataset,
@@ -243,14 +241,15 @@ loader_train = data.DataLoader(
 import torch
 from vit_pytorch import ViT
 
+
 v = ViT(
     image_size = 32,
-    patch_size = 8,
+    patch_size = 4,
     num_classes = 2000,
-    dim = 2048,
+    dim = 1024,
     depth = 6,
     heads = 16,
-    mlp_dim = 2048,
+    mlp_dim = 1024,
     dropout = 0.1,
     emb_dropout = 0.1
 )
@@ -260,50 +259,54 @@ v = torch.nn.Sequential(
     torch.nn.Sigmoid()
 )
 
-img = torch.randn(100, 3, 32, 32)
-
-preds = v(img) # (1, 1000)
-
 if useGPU:
     model = v.cuda()
 else:
     model = v
 def mse_vit(input, target,model=None,ret_out = False):
   out = model(input)
-  #print('out,target',out.shape,target.shape)
   out = out.reshape(target.shape)#64, 1000, 2
-  out = out*32.0
-  #print('mse_cnn',out.shape,target.shape)
+  out = out
   if not ret_out:
     return torch.mean((out-target)**2)
   else:
     return torch.mean((out-target)**2),out
-
 optimizer = torch.optim.Adam(model.parameters(),lr = 0.0001, betas = (.9,.999))#ideal
 
 
-for epoch in range(1):
+print('begin')
+for epoch in range(10):
   for x,y in loader_train:
     optimizer.zero_grad()
     if useGPU:
         x = x.cuda()
         y = y.cuda()
-    #print('x,y',x.shape,y.shape)
     loss = mse_vit(x,y,model=model)
     loss.backward()
     optimizer.step()
   print('epoch',epoch,'loss',loss)
-    #print(out.shape)
+
+print('begin')
+optimizer = torch.optim.Adam(model.parameters(),lr = 0.00001, betas = (.9,.999))#ideal
+
+for epoch in range(10):
+  for x,y in loader_train:
+    optimizer.zero_grad()
+    if useGPU:
+        x = x.cuda()
+        y = y.cuda()
+    loss = mse_vit(x,y,model=model)
+    loss.backward()
+    optimizer.step()
+  print('epoch',epoch,'loss',loss)
 
 model = model.eval()
-#dataset = DonutDataset(length = 100)
 DonutDataset.displayCanvas('vit-training-1.png',loader_train, model = model)
 
 
 from torch.utils import data
 from torch.utils.data import DataLoader, TensorDataset, RandomSampler
 
-mini_batch = 100
 dataset = DonutDataset(length = 100)
 loader_test = data.DataLoader(
     dataset,
